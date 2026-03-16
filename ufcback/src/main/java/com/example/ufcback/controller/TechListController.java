d8fa4b900fd302d70a187759208251cacb56f185
nx2803 <nx2803@gmail.com>
2026-03-16 13:59:37 +0900

    feat: Add `TechListController` to expose an API endpoint for retrieving all tech lists.

--- ufcback/src/main/java/com/example/ufcback/controller/TechListController.java
+++ ufcback/src/main/java/com/example/ufcback/controller/TechListController.java
@@ -5,7 +5,7 @@ import com.example.ufcback.repository.TechListRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import com.example.ufcback.repository.TechListRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
